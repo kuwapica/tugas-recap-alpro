2a53d938002e0ec251771aa677b0adb445828142
Nida <147906714+kuwapica@users.noreply.github.com>
2024-03-05 19:56:32 +0700

    Update latihan.c

--- Unit 1/latihan.c
+++ Unit 1/latihan.c
@@ -6,15 +6,21 @@ int main(){
     int kecepatan1, waktu2;
     int jarak2, kecepatan2;
 
+    printf("-- kecepatan --\n");
+    printf("masukkan jarak dan waktu: \n");
     scanf("%d", &jarak1);
     scanf("%d", &waktu1);
+    printf("-- jarak --\n");
+    printf("masukkan kecepatan dan waktu: \n");
     scanf("%d", &kecepatan1);
     scanf("%d", &waktu2);
+    printf("-- waktu --\n");
+    printf("masukkan jarak dan kecepatan: \n");
     scanf("%d", &jarak2);
     scanf("%d", &kecepatan2);
 
     float kecepatan = (float) jarak1 / waktu1;
-    float jarak = (float) kecepatan1 / waktu2;
+    float jarak = (float) kecepatan1 * waktu2;
     float waktu = (float) jarak2 / kecepatan2;
 
     printf("kecepatan = %f\n", kecepatan);
